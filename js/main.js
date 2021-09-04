96b40055159ddc956aad687269d100cf3f6d43ad
jegor377 <jegor377@gmail.com>
2021-09-05 00:18:00 +0200

    Made hamburger menu for small screen devices.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,84 @@
+var APP = function() {
+	const hamburger_menu_btn = document.getElementById("hamburger-menu-btn")
+	const nav_links = document.getElementById("nav-links")
+
+	const navigation_anim_time = 300;
+
+	for(let i = 0; i < nav_links.children.length; i++) {
+		nav_links.children[i].style.animationDelay = (i + 1) * 100 + "ms"
+		nav_links.children[i].addEventListener("click", (event) => {
+			hide_navigation();
+		})
+	}
+
+	hamburger_menu_btn.addEventListener("click", (event) => {
+		show_navigation();
+	})
+
+	function show_navigation() {
+		nav_links.classList.add("nav-links-on-shown")
+		nav_links.classList.remove("nav-links-on-hidden")
+		nav_links.animate([
+			{
+				transform: 'translateY(100px)',
+				opacity: 0,
+			},
+			{
+				transform: 'translateY(0px)',
+				opacity: 1,
+			}
+		], {
+			easing: 'ease-out',
+			duration: navigation_anim_time,
+		})
+		animate_showing_navigation_links();
+	}
+
+	function animate_showing_navigation_links() {
+		for(let i = 0; i < nav_links.children.length; i++) {
+			let nav_link = nav_links.children[i];
+			nav_link.style.opacity = 0;
+			setTimeout(() => {
+				let anim = nav_link.animate([
+					{
+						transform: 'translateX(-100px)',
+						opacity: 0,
+					},
+					{
+						transform: 'translateX(0px)',
+						opacity: 1,
+					}
+				], {
+					easing: 'ease-out',
+					duration: navigation_anim_time,
+				})
+				anim.onfinish = () => {
+					nav_link.style.opacity = 1;
+				}
+			}, (i + 1) * 100)
+		}
+	}
+
+	function hide_navigation() {
+		let anim = nav_links.animate([
+			{
+				transform: 'translateY(0px)',
+				opacity: 1,
+			},
+			{
+				transform: 'translateY(100px)',
+				opacity: 0,
+			}
+		], {
+			easing: 'ease-out',
+			duration: navigation_anim_time,
+		})
+		anim.onfinish = () => {
+			nav_links.classList.remove("nav-links-on-shown")
+			nav_links.classList.add("nav-links-on-hidden")
+		}
+	}
+}
+
+APP();
 AOS.init();
